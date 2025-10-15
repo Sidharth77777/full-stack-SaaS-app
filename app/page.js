6888cb9078ba29c1eb6c1e9a004be21764f048a2
Sidharth77777 <sidharthks2004@gmail.com>
2025-10-15 20:15:15 +0530

    setup prisma client and tested

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
-export default function Home() {
+import { prisma } from "@/lib/db"
+
+
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return(
     <div>
       <h1>HI</h1>
+      <pre>{JSON.stringify(users)}</pre>
     </div>
   )
   
